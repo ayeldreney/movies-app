c03cb35c12321eae75834601fb57fb048b0b12bf
ayeldreney <60024298+ayeldreney@users.noreply.github.com>
2023-01-30 21:29:33 +0200

    ini project

---
+++ lib/layout/homeScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String ROUTENAME = "homescreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("dasdas")),
+    );
+  }
+}
